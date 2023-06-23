b2481a964b7eaaa193b62b8ffd382e6d7efad19a
ahsa91 <ahsa91@gmail.com>
2023-06-23 23:54:23 +0100

    Bug Fix - make action icons on movie card configurable.

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -11,7 +11,6 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 import { MoviesContext } from "../../contexts/moviesContext";
@@ -25,7 +24,7 @@ const styles = {
   },
 };
 
-export default function MovieCard({ movie }) {      // Destructure props
+export default function MovieCard({ movie, action }) {
   const { favourites, addToFavourites } = useContext(MoviesContext);
 
   if (favourites.find((id) => id === movie.id)) {
@@ -34,10 +33,6 @@ export default function MovieCard({ movie }) {      // Destructure props
     movie.favourite = false
   }
 
-  const handleAddToFavourite = (e) => {
-    e.preventDefault();
-    addToFavourites(movie);
-  };
 
   return (
     <Card sx={styles.card}>
@@ -80,9 +75,7 @@ export default function MovieCard({ movie }) {      // Destructure props
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}>
-            <FavoriteIcon color="primary" fontSize="large" />
-          </IconButton>
+      {action(movie)}
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,9 +3,10 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
 
-const HomePage = (props) => {
-  const { data, error, isLoading, isError } = useQuery("discover", getMovies);
+const HomePage = () => {
+    const { data, error, isLoading, isError } = useQuery("discover", getMovies);
 
   if (isLoading) {
     return <Spinner />;
@@ -16,16 +17,15 @@ const HomePage = (props) => {
 
   const movies = data ? data.results : [];
 
-  // Redundant, but necessary to avoid app crashing.
-  const favourites = movies.filter((m) => m.favorite);
-  localStorage.setItem("favourites", JSON.stringify(favourites));
-  const addToFavourites = (movieId) => true;
+  
 
   return (
     <PageTemplate
       title="Discover Movies"
       movies={movies}
-      selectFavourite={addToFavourites}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
 };
