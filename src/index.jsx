377060a917f9662c1afeb21aba348201f6c5028b
ahsa91 <ahsa91@gmail.com>
2023-06-23 23:19:03 +0100

    Using react-query to manage the browser-based cache.

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,26 @@ import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import UpcomingPage from "./pages/upcomingPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 
 
 const App = () => {
   return (
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-          <SiteHeader />      {/* New Header  */}
           <Routes>
               <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
               <Route path="/movies/:id" element={<MoviePage />} />
@@ -23,7 +35,8 @@ const App = () => {
               <Route path="/reviews/:id" element={<MovieReviewPage/>} />
               <Route path="/movies/upcoming" element={<UpcomingPage/>} />
             </Routes>
-    </BrowserRouter>
+        </BrowserRouter>
+        </QueryClientProvider>
   );
 };
 
