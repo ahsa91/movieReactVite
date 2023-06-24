32e74f5f506109b72fc2b30053dd859ce6368bbd
ahsa91 <ahsa91@gmail.com>
2023-06-24 01:32:27 +0100

    Add new review support

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,8 @@ import UpcomingPage from "./pages/upcomingPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 
 
 
@@ -38,6 +40,7 @@ const App = () => {
               <Route path="*" element={<Navigate to="/" />} />
               <Route path="/reviews/:id" element={<MovieReviewPage/>} />
               <Route path="/movies/upcoming" element={<UpcomingPage/>} />
+              <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
               </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
